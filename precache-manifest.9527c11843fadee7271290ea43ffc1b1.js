e8a894053beab56f6e695afd2fca6ad0618dad3e
Sthitadhi Roy <sthitadhiroy@Sthitadhis-MacBook-Air.local>
2020-04-17 13:09:13 +0530

    Updates

---
+++ precache-manifest.9527c11843fadee7271290ea43ffc1b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adaad57851d345ce318641773d0d83f0",
+    "url": "/COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "81fbd5d5d330de73f0a6",
+    "url": "/COVID19-Tracker/static/css/2.e50de653.chunk.css"
+  },
+  {
+    "revision": "949acef6e9e217897065",
+    "url": "/COVID19-Tracker/static/css/main.b0e6e590.chunk.css"
+  },
+  {
+    "revision": "81fbd5d5d330de73f0a6",
+    "url": "/COVID19-Tracker/static/js/2.aec913c5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19-Tracker/static/js/2.aec913c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949acef6e9e217897065",
+    "url": "/COVID19-Tracker/static/js/main.ab83d932.chunk.js"
+  },
+  {
+    "revision": "16ab2a09abd09f6e5089",
+    "url": "/COVID19-Tracker/static/js/runtime-main.67020a01.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19-Tracker/static/media/image.d7265326.png"
+  }
+]);
